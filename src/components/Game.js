0794cd8dec6ca1dc086f965175783b3d2a36a0d5
Webster Wing <webster.wing@gmail.com>
2021-01-05 14:51:00 -0600

    update axios to avoid vulerability

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,38 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Board from './Board'
-import {calculateWinner} from '../helper'
+import socket from '../socket'
 
 export default function Game() {
   const [moves, setMoves] = useState([])
-  const [xIsNext, setXIsNext] = useState(true)
-
   const squares = calculateSquares(moves)
-  const winner = calculateWinner(squares)
+  const [side, setSide] = useState('X')
+  const [winner, setWinner] = useState(null)
 
   const onClick = i => {
-    if (squares[i] || winner) return
-    const move = {square: i, value: xIsNext ? 'X' : 'O'}
-    setMoves(moves => [...moves, move])
-    setXIsNext(!xIsNext)
+    axios.post('/move', 
+      {
+        move: {value: side, square: i, sequence_num: moves.length},
+        id: "placeholder-uuid",
+      }
+    ).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   }
 
-  const reset = () => {
-    setMoves([])
-    setXIsNext(true)
+  const changeSide = () => {
+    if (side === 'X') {
+      setSide('O')
+    } else {
+      setSide('X')
+    }
   }
+
+  useEffect(() => {
+    console.log('setting up listener')
+    // Listener must be declared seperately so that it can be cleaned up
+    const listener = (newMoves, newWinner) => {
+      setMoves(newMoves)
+      setWinner(newWinner)
+    }
+    socket.on('game-update', listener)
+    return () => socket.off('game-update', listener)
+  }, [])
 
   return (
     <>
-
+    <button className={`sideToggle ${side}`} onClick={changeSide}>
+      You are on side {side} (click to change)
+    </button>
     <Board 
       onClick={onClick}
       squares={squares}
     />
-    <h3>
+    <h3>    
+    { /*
     {!winner && `${xIsNext ? 'X' : 'O'} plays now`}
+    <button onClick={reset}>reset</button>
+    */ }    
     {winner && `🥳${winner} HAS WON🥳`}
     </h3>
-    <button onClick={reset}>reset</button>
+    
+
     </>
   )
 }
